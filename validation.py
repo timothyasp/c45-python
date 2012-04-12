8eeb9606e375b9de7e86fe8aacf7a83ef702aed2
Timothy Asp <timothy.asp@gmail.com>
2012-04-12 10:34:48 -0700

    added file parsing to validation

--- validation.py
+++ validation.py
@@ -4,6 +4,25 @@ import xml.dom.minidom
 import csv
 
 def main():
+    num_args = len(sys.argv)
+
+    # Make sure the right number of input files are specified
+    if  num_args < 3 or num_args > 4:
+        print 'Expected input format: python classify.py <csvFile> <XMLFile>'
+        return
+    # If they are read them in
+    else: 
+        if num_args == 4:
+            restrictions = open(sys.argv[3], 'r')
+            print restrictions
+        csv_reader = csv.reader(open(sys.argv[1], 'r'))
+        n_fold = int(sys.argv[2])
+
+        print "N Folds: ", n_fold
+        for row in csv_reader:
+            print row
+
+
         
 
 if __name__ == '__main__':
